e4ff8a48ace3fd160eb3c433b44e060f699b3037
BidhanCR <bidhanroy300805@gmail.com>
2023-05-01 21:54:53 +0600

    folder structure created

---
+++ src/Routes/PrivateRoutes.jsx
@@ -0,0 +1,12 @@
+/* eslint-disable no-unused-vars */
+import React from 'react';
+
+const PrivateRoutes = () => {
+    return (
+        <div>
+            
+        </div>
+    );
+};
+
+export default PrivateRoutes;
