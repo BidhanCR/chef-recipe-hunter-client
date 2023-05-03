9d1485eb4e33e1e6511b12c87995e45124dcbe5a
BidhanCR <bidhanroy300805@gmail.com>
2023-05-03 06:36:30 +0600

    login and register form added

--- src/components/Login/Login.jsx
+++ src/components/Login/Login.jsx
@@ -1,12 +1,49 @@
 /* eslint-disable no-unused-vars */
-import React from 'react';
+import React from "react";
+import { Link } from "react-router-dom";
 
 const Login = () => {
-    return (
-        <div>
-            
+  return (
+    <div className="hero min-h-screen bg-base-200">
+      <div className="hero-content flex-col lg:flex-col">
+        <div className="text-center lg:text-left">
+          <h1 className="text-5xl font-bold mb-8">Login now!</h1>
         </div>
-    );
+        <div className="card flex-shrink-0 w-full max-w-sm shadow-2xl bg-base-100">
+          <div className="card-body">
+            <div className="form-control">
+              <label className="label">
+                <span className="label-text">Email</span>
+              </label>
+              <input
+                type="text"
+                placeholder="email"
+                className="input input-bordered"
+              />
+            </div>
+            <div className="form-control">
+              <label className="label">
+                <span className="label-text">Password</span>
+              </label>
+              <input
+                type="text"
+                placeholder="password"
+                className="input input-bordered"
+              />
+              <label className="label">
+                <p className="label-text">
+                  New to FoodieFrenzy? Please <Link className="link" to='/register'>Register</Link>
+                </p>
+              </label>
+            </div>
+            <div className="form-control mt-6">
+              <button className="btn bg-[#71bd46] border-[#71bd46] hover:bg-[#a8d969]">Login</button>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
 };
 
 export default Login;
